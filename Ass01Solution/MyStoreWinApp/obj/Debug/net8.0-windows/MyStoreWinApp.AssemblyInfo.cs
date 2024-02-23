f119306ba1b1c15ca2ffc4a57d524858a4c37d1a
Ngo Bao Lam <lamnbhe170781@fpt.edu.vn>
2024-02-23 10:13:12 +0700

    feat: completed lab2

--- Ass01Solution/MyStoreWinApp/obj/Debug/net8.0-windows/MyStoreWinApp.AssemblyInfo.cs
+++ Ass01Solution/MyStoreWinApp/obj/Debug/net8.0-windows/MyStoreWinApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyStoreWinApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d8b2a252b0878acde35f419167684ab532a8826")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyStoreWinApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyStoreWinApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
